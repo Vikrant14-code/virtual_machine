38fd4be10cf6d13b6b70502cace440e3e595ea61
Vikrant14-code <vikrantpandey0707@gmail.com>
2025-07-22 22:28:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,66 +2,56 @@ resource "azurerm_resource_group" "resourcegroup" {
   name     = "vikrant-resources"
   location = "eastasia"
 }
+
 resource "azurerm_virtual_network" "vnet" {
-  depends_on          = [azurerm_resource_group.resourcegroup]
   name                = "vikrant-network"
   resource_group_name = azurerm_resource_group.resourcegroup.name
   location            = azurerm_resource_group.resourcegroup.location
   address_space       = ["10.0.0.0/16"]
-
-
 }
+
 resource "azurerm_subnet" "subnet" {
-    depends_on = [ azurerm_virtual_network.vnet ]
   name                 = "internal-subnet"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
-  virtual_network_name = "vikrant-network"
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
 resource "azurerm_public_ip" "pip" {
   name                = "frontend-ip"
   resource_group_name = azurerm_resource_group.resourcegroup.name
-  location            = "eastasia"
+  location            = azurerm_resource_group.resourcegroup.location
   allocation_method   = "Static"
-   
 }
+
 resource "azurerm_network_interface" "nic" {
-  depends_on          = [azurerm_virtual_network.vnet]
   name                = "frontend-nic"
-  location            = "eastasia"
+  location            = azurerm_resource_group.resourcegroup.location
   resource_group_name = azurerm_resource_group.resourcegroup.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-      public_ip_address_id = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  depends_on                      = [azurerm_network_interface.nic]
   name                            = "frontend-machine"
   resource_group_name             = azurerm_resource_group.resourcegroup.name
-  location                        = "eastasia"
+  location                        = azurerm_resource_group.resourcegroup.location
   size                            = "Standard_F2"
   admin_username                  = "adminuser"
   admin_password                  = "Vikrant@141194"
   disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
 
-  #   admin_ssh_key {
-  #     username   = "adminuser"
-  #     public_key = file("~/.ssh/id_rsa.pub")
-  #   }
+  network_interface_ids = [ azurerm_network_interface.nic.id ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
     disk_size_gb         = 30
-
   }
 
   source_image_reference {
